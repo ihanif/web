06e000459d4b213fd8031a76e87e5ec5bf6873ef
Hanif Rahman <hanifkhan@gmail.com>
2023-03-06 21:49:51 +0000

    fix: use text-lg for blog posts

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -97,7 +97,8 @@ export default function PostLayout({
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+              {/* todo: change copy font size here */}
+              <div className="prose max-w-none pt-10 pb-8 text-xl dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(slug)} rel="nofollow">
                   {t('common:twitter')}
